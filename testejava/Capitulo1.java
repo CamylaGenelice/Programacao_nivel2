c66a898d6603529d3838e9f920429fab7c26cca8
Camyla Genelice <genelicecamyla@gmail.com>
2023-11-07 07:15:15 -0300

    Modificacoes
    Um capitulo novo foi criado e algumas coisas foram adicionadas na classe personagem.

---
+++ testejava/Capitulo1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+//import javax.sound.sampled.*;
+//import java.net.URL;
+public class Capitulo1 {
+    HistoriaPersonagem objeto = new HistoriaPersonagem();
+    private String nome;
+    private String texto;
+    private String escolha1;
+    private String escolha2;
+    private int energia;
+
+
+    public int getEnergia() {
+        return energia;
+        
+    }
+
+
+
+
+
+    public Capitulo1(String nome, String texto, String escolha1, String escolha2, int energia) {
+        this.nome = nome;
+        this.texto = texto;
+        this.escolha1 = escolha1;
+        this.escolha2 = escolha2;
+        this.energia = energia;
+    }
+    
+    public void mostrar(){
+        System.out.println("Escolha 1: ");
+        System.out.println("Quando Emily abriu a porta, so o suficiente para olhar o comodo, não tinha nada que pudesse ter causado o barulho. Então ela fechou a porta e desceu as escadas rumo a sala. Quando Emily chega na sala, ela percebe que seu livro sumiu. \n \'Que estranho eu me lembro de ter deixado meu livro aqui. /'  ");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        HistoriaPersonagem objeto = new HistoriaPersonagem();
+        Historia.printLinha("Bem vindo ao Capítulo 1!");
+        System.out.println("Bem vindo ao Capitulo 1");
+        
+    }
+}
--- testejava/HistoriaPersonagem.java
+++ testejava/HistoriaPersonagem.java
@@ -1,8 +1,8 @@
 public class HistoriaPersonagem {
-    // *Atributos*
-    String nome;
-    int energia;
     
+    // *Atributos da Emily*
+    public static int vitalidade = 100;
+    public String nome = "Emily";
     // *Métodos*
     void seEsconder(){
         System.out.println("....");
@@ -13,13 +13,29 @@ public class HistoriaPersonagem {
     void apagarLanterna(){
         System.out.println("----0");
     }
-    public class TesteHistoria{
-    public static void main(String[] args){
-        
+
+     // Atributos do killer
+    int visao = 100;
+
+    // *Métodos do killer *
+    void andar (){
+        System.out.println("rew rew rew...");
     }
-    
-
+    static int dano = 0;
+    public static void assustar() {
+        if (vitalidade >= 10) {
+            vitalidade -= 10;
+            dano = vitalidade;
+            System.out.println("Você foi assustado e perdeu 10 pontos de vitalidade. Vitalidade atual: " + dano);
+            //System.out.println("oi"+dano);
+        }
+}
+    public static void main(String[] args){
+        System.out.println("Vitalidade antes de ser assustado: " + vitalidade);
+        assustar();
+        System.out.println("Vitalidade depois de ser assustado: " + vitalidade);
+    }
 
 }
 
-}
+
